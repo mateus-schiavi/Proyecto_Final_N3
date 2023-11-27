033636fdc70f48da23899ffdb4c50e8f3d36111d
Mateus Schiavi <mtsschiavi@gmail.com>
2023-11-27 17:47:18 -0300

    updated database

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "university_db";
+$dbname = "db_university";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
